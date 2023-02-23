e36521ed591473ff641a2f1c82f6dad2cb44ca8a
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-02-23 08:48:48 -0800

    delete node

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,7 +43,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
+		tmp = (*head)->next;
+		(*head)->prev->next = tmp;
 		free(*head);
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;

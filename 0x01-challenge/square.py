54bb159d794f4cd6044312759d52ae7030a3e4e8
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-18 16:38:37 -0700

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-"""class"""
+""" some docstring for the checker """
 
 
 class square():
-    """Ä square"""
+    """ some docstring for the checker """
+    width = 0
+    height = 0
 
     
-    size = 0
-    
     def __init__(self, *args, **kwargs):
+        """ Instantiation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.size ** 2
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.size * 4)
+        """"Perimeter"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.size, self.size)
+    """str"""
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = square(size=12)
+    """ some docstring for the checker """
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())

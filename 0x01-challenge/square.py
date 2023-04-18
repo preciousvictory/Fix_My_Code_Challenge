950ad3094a310e70936f03e7069ed5a917052623
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-18 16:55:05 -0700

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,8 +16,8 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def permiterOfMySquare(self):
-        """"Perimeter"""
+    def permiter_of_my_square(self):
+        """Perimeter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiterOfMySquare())
+    print(s.permiter_of_my_square())
